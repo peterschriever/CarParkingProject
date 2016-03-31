a54e20eecb0f9114af16227aa2e9d5ff92d41923
Peter Schriever <peterzen72@gmail.com>
2016-03-31 19:18:09 +0200

    added start of project readme file

---
+++ src/nl/hanze/carparksimulator/Main.java
@@ -0,0 +1,13 @@
+package nl.hanze.carparksimulator;
+
+/**
+ * Created by peterzen on 3/31/16.
+ * Part of the javaTest project.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Simulator sim = new Simulator();
+        sim.run();
+    }
+}
