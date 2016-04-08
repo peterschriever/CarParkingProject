1f461291c3d57b9f6e3f0b84cf8f668d9c838012
Gabe <glwwitteveen@live.nl>
2016-04-08 12:55:48 +0200

    Added Reservation Class Added getFreeFirstReservatedLocation method

---
+++ src/nl/hanze/carpark/model/Reservation.java
@@ -0,0 +1,11 @@
+package nl.hanze.carpark.model;
+
+/**
+ * Created by Gabe on 8-4-2016.
+ */
+public class Reservation extends Car {
+
+    public Reservation() {
+
+    }
+}
--- src/nl/hanze/carpark/view/CarParkView.java
+++ src/nl/hanze/carpark/view/CarParkView.java
@@ -6,6 +6,7 @@ import nl.hanze.carpark.main.CarPark;
 import nl.hanze.carpark.model.AdHocCar;
 import nl.hanze.carpark.model.Car;
 import nl.hanze.carpark.model.Location;
+import nl.hanze.carpark.model.ParkingPassCar;
 
 import javax.swing.*;
 import java.awt.*;
@@ -51,27 +52,33 @@ public class CarParkView extends AbstractView {
 
         Graphics graphics = carParkImage.getGraphics();
 
-        for (int floor = 0; floor < numberOfFloors ; floor++) {
-            for (int row = 0; row < numberOfRows ; row++) {
-                for (int place = 0; place < numberOfPlaces ; place++) {
+        for (int floor = 0; floor < numberOfFloors; floor++) {
+            for (int row = 0; row < numberOfRows; row++) {
+                for (int place = 0; place < numberOfPlaces; place++) {
                     Location location = new Location(floor, row, place);
                     Car car = ((SimulatorController) simController).getCarAt(location);
                     Color color;
                     if (car == null) {
-                        color = Color.WHITE;
-                    } else {
-                        if (car instanceof AdHocCar) {
-                            color = Color.RED;
+                        if(floor == 2) {
+                            color = Color.ORANGE;
                         } else {
-                            color = Color.BLUE;
+                            color = Color.WHITE;
                         }
+                    } else if(car instanceof AdHocCar) {
+                        color = Color.RED;
+                    } else if(car instanceof ParkingPassCar) {
+                        color = Color.BLUE;
+                    } else {
+                        color = Color.MAGENTA;
                     }
+
                     drawPlace(graphics, location, color);
                 }
             }
+
+            setVisible(true);
+            super.updateView();
         }
-        setVisible(true);
-        super.updateView();
     }
 
     private void drawPlace(Graphics graphics, Location location, Color color) {
